9a5649a3d0a56046e5b1a449f873f06398807caf
swapnil patil <swapnilpatil8771@gmail.com>
2025-06-17 14:43:54 +0530

    Folder structure updated

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+   <>
+   <div>
+   hi
+         <h1 className="text-3xl font-bold text-blue-600">Tailwind is Working!</h1>
+
+   </div>
+   </>
+  )
+}
+
+export default App
